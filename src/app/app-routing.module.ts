2834462f51ea6bfdd489ddb614bf136519efe16d
mayurrohokale <mayurrohokale12345@gmail.com>
2023-09-22 22:40:59 +0530

    added new component called for Update Method

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
 import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/employees/edit-employee/edit-employee.component';
 
 const routes: Routes = [
 
@@ -10,13 +11,18 @@ const routes: Routes = [
     component: EmployeesListComponent
   },
 
-  {
-    path: '',
-    component: EmployeesListComponent
-  },
+  // {
+  //   path: '',
+  //   component: EmployeesListComponent
+  // },
   {
     path: 'employees/add',
     component: AddEmployeeComponent
+  },
+
+  {
+    path: 'employees/edit/:id',
+    component: EditEmployeeComponent
   }
 ];
 
